896fc065a5ebc1874c88f372229e65dd2cb6b956
tan-ishq21 <srivastavatan17@gmail.com>
2024-02-11 11:20:37 +0530

    footer-mar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
     return (
         <>
-        <div className="footer bg-dark">
+        <div className="footer bg-dark mt-3">
         <footer className="d-flex flex-wrap justify-content-center align-items-center py-3 border-top">
             <div className="col-md-4 d-flex justify-content-center align-items-center">
             <Link to="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
